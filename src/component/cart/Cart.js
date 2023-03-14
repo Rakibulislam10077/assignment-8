a4aea8729e337d55ebb3691ea285e12ab53f36ed
Rakibulislam10077 <devs.rakibulislam@gmail.com>
2023-03-14 18:19:55 +0600

    new style added

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart, handleRemoveProduct }) => {
 
     return (
         <div>
             {
-                cart.map(product => <p>{product.name} <button onClick={() => handleRemoveProduct(product)}>X</button></p>)
+                cart.map(product => <p className='cartBtn'>{product.name} <button onClick={() => handleRemoveProduct(product)}>X</button></p>)
             }
         </div>
     );
